38925b6dc105ec39695f35e070da8cfa3b74cb74
Victor <hdvictor176@gmail.com>
2026-03-10 09:21:07 +0100

    Lobby WIP 1

--- Assets/Scripts/Cinturon y objetos/CinturonPlayerCoord.cs
+++ Assets/Scripts/Cinturon y objetos/CinturonPlayerCoord.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CinturonPlayerCoord : MonoBehaviour
@@ -6,6 +7,8 @@ public class CinturonPlayerCoord : MonoBehaviour
     public GameObject jugador;
 
     private ControlesExternos controles;
+
+    public float desfaseRotacion;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,5 +20,7 @@ public class CinturonPlayerCoord : MonoBehaviour
     void Update()
     {
         this.gameObject.transform.position = jugador.transform.position + controles.posicionCabeza - new Vector3(0, 0.4f, 0);
+
+        this.gameObject.transform.rotation = quaternion.Euler(jugador.transform.rotation.x + controles.rotacionCabeza.x, jugador.transform.rotation.y + controles.rotacionCabeza.y + desfaseRotacion, jugador.transform.rotation.z + controles.rotacionCabeza.z);
     }
 }
